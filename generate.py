5d9e70401e11bbef33a20d34513c67027e2a1dd2
Hemant Sirsat <hemantsirsat40@gmail.com>
2022-08-12 00:54:06 +0200

    minor changes

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
 import sys
-import string
 import random
 import pandas as pd
 from burrows import Burrows
@@ -7,20 +6,19 @@ from burrows import Burrows
 def generate_dataset(csv_name,rows):
     pure_str = []
     bwt_list = []
-    for i in range(0,rows):
-        genome = ''.join(random.choice(string.ascii_uppercase) for _ in range(random.randint(6,8)))
-        # b = Burrows(genome)
-            # bwt_str = b.bwt()
-        pure_str.append(genome)
-            # bwt_list.append(bwt_str)
 
-    pure_str = list(set(pure_str))
+    while len(pure_str) != rows:
+        genome = ''.join(random.choice(['A','T','G','C']) for _ in range(random.randint(5,9)))
+        pure_str.append(genome)
+
+        pure_str = list(set(pure_str))
+
     bwt_list = [Burrows(x).bwt() for x in pure_str]
 
-    # bwt_list = list(set(bwt_list))
     df = pd.DataFrame()
     df['String'] = pure_str
     df['BWT_string'] = bwt_list
+
     if '.csv' not in csv_name:
         csv_name = f'{csv_name}.csv'
     df.to_csv(csv_name,index=False)
